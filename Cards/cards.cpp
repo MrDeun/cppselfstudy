a6610385a0ecd22ed791703bd1414bb05a5f2b51
MrDeun <pmichalak16@gmail.com>
2023-06-28 01:14:27 +0200

    first project start - cards
    Create a simulation of playing cards. Created cards.cpp which has a definition of a card.

---
+++ Cards/cards.cpp
@@ -0,0 +1,15 @@
+
+class Card
+{
+    private:
+        enum class Value {Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten,Jack,Queen,King};
+        enum class Color {Club,Diamond,Heart,Spade};
+    public:
+};
+
+
+
+int main()
+{
+    return 0;
+}
