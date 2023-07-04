2265f1b38a2a93f1ba168777c985410d2f4b5e2e
MrDeun <pmichalak16@gmail.com>
2023-07-04 11:29:04 +0200

    Cards - 0.03
    NOTE: Stacks are not good when comes to doing something with them.

--- Cards/cards.cpp
+++ Cards/cards.cpp
@@ -1,10 +1,13 @@
 #include<vector>
 #include<cstdlib>
 #include<iostream>
+#include<algorithm>
 class Card
 {
+    friend std::ostream& operator<<(std::ostream& Output, Card& Card_in);
+
     private:
-        enum class libValue {Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten,Jack,Queen,King};
+        enum class libValue {Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten,Jack,Queen,King,Ace};
         enum class libColor {Club,Diamond,Heart,Spade};
         int Color;
         int Value; 
@@ -36,7 +39,7 @@ class Deck
         Deck()
         {
             for(int iterator_color=0;iterator_color<4;iterator_color++)
-                for(int iterator_value=0;iterator_value<12;iterator_value<12)
+                for(int iterator_value=0;iterator_value<13;iterator_value++)
                 {
                     Card placeholder(iterator_value,iterator_color);
                     DeckGame.push_back(placeholder);
@@ -50,14 +53,49 @@ class Deck
 
         void Show()
         {
-         for(int i=0;i<DeckGame.size();i++)
-            std::cout<<i+1<<" "<<DeckGame[i].getColor()<<" "<<DeckGame[i].getValue()<<std::endl;
+            int iterator=1;
+        for(Card card_iterator : DeckGame)
+            {
+                std::cout<<iterator<<". "<<card_iterator<<std::endl;
+                iterator++;
+            }
+        }
+
+        void Shuffle()
+        {
+            std::random_shuffle(DeckGame.begin(),DeckGame.end());
+        }
+
+        void RiffleShuffle()
+        {
+            std::vector<Card> temp;
+            temp.reserve(DeckGame.size());
+
+            for (int i = 0; i < DeckGame.size()/2; i++)
+                temp.push_back(DeckGame[i*2]);
+            for (int i = 0; i < DeckGame.size()/2; i++)
+                temp.push_back(DeckGame[(i*2)+1]);
+
+            std::copy(temp.begin(),temp.end(),DeckGame.begin());
+
+            delete &temp;
+            return;
+            
+            
         }
 };
+
+std::ostream& operator<<(std::ostream& Output, Card& Card_in)
+{
+    Output<<Card_in.getColor()<<" "<<Card_in.getValue();
+    return Output;
+}
 
 int main()
 {
     Deck DeckOne;
+    DeckOne.Show();
+    DeckOne.Shuffle();
     DeckOne.Show();
     getchar();
     return 0;
