d7e39f281795e71ef53df08efe9b05a20fcc2ab4
MrDeun <59258041+MrDeun@users.noreply.github.com>
2024-01-07 16:24:45 +0100

    Save and Load function implemented

--- MiniThings/ToDoList/test/pointer_menu/pointer_menu.cpp
+++ MiniThings/ToDoList/test/pointer_menu/pointer_menu.cpp
@@ -1,84 +1,79 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
 
-void clear_screen()
-{
-	std::cout << "\033[2J\033[1;1H";
-	return;
+#include <conio.h>
+
+void clear_screen() {
+  std::cout << "\033[2J\033[1;1H";
+  return;
 }
 
-void view_vector(const std::vector<std::string>& tasklist, std::string* ptr_task)
-{
-	clear_screen();
-	for (size_t i = 0; i < tasklist.size(); i++)
-	{
-		if (ptr_task == &tasklist[i])
-		{
-			std::cout << ">";
-		}
-		
-		else
-		{
-			std::cout << " ";
-		}
+void view_vector(const std::vector<std::string> &tasklist,
+                 std::string *ptr_task) {
+  clear_screen();
+  for (size_t i = 0; i < tasklist.size(); i++) {
+    if (ptr_task == &tasklist[i]) {
+      std::cout << ">";
+    }
 
-		std::cout << i+1 << ". "<< tasklist[i] << "\n";
-	}
+    else {
+      std::cout << " ";
+    }
+
+    std::cout << i + 1 << " . " << tasklist[i] << "\n";
+  }
 }
 
-void display(const std::string& this_string)
-{
-    clear_screen();
-    std::cout << "You chose: " <<this_string <<'\n';
-    int pause = getchar(); // Just for pause;
-    return;
+void display(const std::string &this_string) {
+  clear_screen();
+  std::cout << "You chose: " << this_string << '\n';
+  int pause = getch();
+  std::cout << pause; // Just for pause;
+  return;
 }
 
-void choose_options(std::vector<std::string>& tasklist) //TO-DO
+void choose_options(std::vector<std::string> &tasklist) // TO-DO
 {
-	int64_t index = 0;
-	std::string* highlighted_task = &tasklist[index];
-	int choice;
-	do
-	{
-		view_vector(tasklist,highlighted_task);
-		choice = getchar();
-		switch(choice)
-		{
-			case 'w':
-				if( (index-1) < 0) break;
-				index--;
-				highlighted_task--;
-				break;
-			case 's':
-				if( (index+1) > tasklist.size()-1) break;
-				index++;
-				highlighted_task++;
-				break;
-			case 'd':
-				display(tasklist[index]);
-				break;
-			case 'q':
-				break;
-            default:
-                break;
+  size_t index = 0;
+  std::string *highlighted_task = &tasklist[index];
+  int choice;
+  do {
+    view_vector(tasklist, highlighted_task);
+    choice = getch();
+    switch (choice) {
+    case 'w':
+      if ((index - 1) < 0)
+        break;
+      index--;
+      highlighted_task--;
+      break;
+    case 's':
+      if ((index + 1) > tasklist.size() - 1)
+        break;
+      index++;
+      highlighted_task++;
+      break;
+    case 'd':
+      display(tasklist[index]);
+      break;
+    case 'q':
+      break;
+    default:
+      break;
+    }
+  } while (choice != 'q');
 
-				
-		}
-	} while (choice != 'q');
-
-	highlighted_task = nullptr;
-	delete highlighted_task;
-    return;
+  highlighted_task = nullptr;
+  delete highlighted_task;
+  return;
 }
 
+int main() {
+  std::vector<std::string> string_list = {"Hello World", "Sup homie",
+                                          "Damn it Bobby!"};
+  choose_options(string_list);
 
-int main()
-{
-    std::vector<std::string> string_list = {"Hello World", "Sup homie", "Damn it Bobby!"};
-    choose_options(string_list);
-
-    return 0;
+  return 0;
 }
--- MiniThings/ToDolist-v2/main.cpp
+++ MiniThings/ToDolist-v2/main.cpp
@@ -1,119 +1,121 @@
-#include <iostream>
+#include <cctype>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <vector>
-#include <cctype>
 
+
+#include "helper.hpp"
 #include "states.hpp"
 #include "task.hpp"
-#include "helper.hpp"
 
-using std::string;
-using std::vector;
-using std::getline;
-using std::cout;
+
 using std::cin;
-using std::stringstream;
+using std::cout;
+using std::getline;
+using std::ifstream;
 using std::ofstream;
+using std::string;
+using std::stringstream;
+using std::vector;
 
-void save_file(const vector<Task>& tasklist, string path)
-{
-    ofstream file(path);
-    for (Task element : tasklist)
-    {
-        file << element.get_title() << element.get_done();
-    }
-    file.close();
+void save_file(const vector<Task> &tasklist, string path) {
+  ofstream file(path);
+  for (Task element : tasklist) {
+    file << element.get_title() << element.get_done();
+  }
+  file.close();
+  return;
+}
+
+void load_file(vector<Task> &tasklist, string path) {
+  tasklist.clear();
+  tasklist.shrink_to_fit();
+
+  ifstream file(path);
+  if (!file.good()) {
+    std::cout << "Savefile not found! Aborting the load \n";
     return;
+  }
+
+  string task_title{};
+  bool task_state{};
+  while (file.good()) {
+    file >> task_title;
+    Task new_task{task_title};
+    file >> state;
+    new_task.set_done(state);
+    tasklist.push_back(new_task);
+  }
 }
 
-void load_file(vector<Task>& tasklist, string path)
-{
+void create_file(std::string path) {}
 
+void modify_task(vector<Task> &tasklist, size_t tasklist_index) {}
+
+void delete_task(vector<Task> &tasklist, size_t tasklist_index) {
+  tasklist.erase(tasklist.begin() + tasklist);
+  tasklist.shrink_to_fit();
+  return;
+}
+void show_tasks(vector<Task> tasklist) {
+  for (size_t i = 0; i < tasklist.size(); i++) {
+    std::cout << i + 1 << ". " << tasklist[i].state();
+  }
+
+  return;
 }
 
-void create_file(std::string path)
-{
-
-}
-
-void modify_task(vector<Task>& tasklist,size_t tasklist_index)
-{
-
-}
-
-void delete_task(vector<Task>& tasklist, size_t tasklist_index)
-{
-
-}
-void show_tasks(vector<Task> tasklist)
-{
-    for (size_t i = 0; i < tasklist.size(); i++)
-    {
-        std::cout << i+1 <<". " << tasklist[i].state();
+bool process(std::string line, vector<Task> &tasklist) {
+  stringstream command_stream(line);
+  string keyword{};
+  while (command_stream >> keyword) {
+    keyword = str_tolower(keyword);
+    std::string path{};
+    switch (string_to_command[keyword]) {
+    case SAVE:
+      command_stream >> path;
+      save_file(tasklist, path);
+      break;
+    case LOAD:
+      command_stream >> path;
+      load_file(tasklist, path);
+      break;
+    case NEW:
+      command_stream >> path;
+      create_file(path);
+      break;
+    case MODIFY:
+      command_stream >> path;
+      modify_task(tasklist, stoi(path));
+      break;
+    case DELETE:
+      command_stream >> path;
+      delete_task(tasklist, stoi(path));
+      break;
+    case SHOW:
+      show_tasks(tasklist);
+      break;
+    case EXIT:
+      return true;
+      break;
+    default:
+      break;
     }
+    path.clear();
+  }
 
-    return;
+  return false;
 }
 
-bool process(std::string line, vector<Task>& tasklist)
-{
-    stringstream command_stream(line);
-    string keyword{};
-    while (command_stream >> keyword)
-    {
-        keyword = str_tolower(keyword);
-        std::string path{};
-        switch(string_to_command[keyword])
-        {
-        case SAVE:
-            command_stream >> path;
-            save_file(tasklist,path);
-            break;
-        case LOAD:
-            command_stream >> path;
-            load_file(tasklist,path);
-            break;
-        case NEW:
-            command_stream >> path;
-            create_file(path);
-            break;
-        case MODIFY:
-            command_stream >> path;
-            modify_task(tasklist,stoi(path));
-            break;
-        case DELETE:
-            command_stream >> path;
-            delete_task(tasklist,stoi(path));
-            break;
-        case SHOW:
-            show_tasks(tasklist);
-            break;
-        case EXIT:
-            return true;
-            break;
-        default:
-            break;
-        }
-        path.clear();
+int main() {
+  bool quit = false;
+  vector<Task> tasklist{};
+  for (string line; cout << "TO DO LIST > " && getline(cin, line);) {
+    if (!line.empty() && process(line, tasklist)) {
+      quit = false;
+      line.clear();
+      return 0;
     }
-    
-    return false;
-
-}
-
-int main()
-{
-    bool quit = false;
-	vector<Task> tasklist{};
-    for(string line; cout << "TO DO LIST > " && getline(cin,line);)
-    {
-        if ( !line.empty() && process(line,tasklist) )
-        {  
-            quit = false;
-            line.clear();
-            return 0;
-        } 
-    }
-
+  }
 }
